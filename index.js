c79b80e291288e97c295aa0f14c464b6b48c46dc
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-05-24 11:44:34 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    return customerName = 'BOB'
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+   return bestCustomer
+}
+
+const leastFavoriteCustomer = 'toby'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'george'
+    return leastFavoriteCustomer
+}
